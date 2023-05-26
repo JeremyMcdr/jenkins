dd99ea6325f2724cea7cf3e9622cb6cbc823d05d
JeremyMcdr <jeremymacadre@gmail.com>
2023-05-26 15:29:06 +0200

    message

---
+++ src/main/java/bank/Account.java
@@ -0,0 +1,39 @@
+package bank;
+
+public class Account {
+    private int id;
+    private double balance;
+    private double interestRate;
+
+    public Account(int id, double balance, double interestRate) {
+        this.id = id;
+        this.balance = balance;
+        this.interestRate = interestRate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        }
+    }
+
+    public double calculateInterest() {
+        return balance * interestRate;
+    }
+}
---
+++ src/main/java/bank/Bank.java
@@ -0,0 +1,33 @@
+package bank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bank {
+    private List<Account> accounts;
+
+    public Bank() {
+        this.accounts = new ArrayList<>();
+    }
+
+    public void addAccount(Account account) {
+        accounts.add(account);
+    }
+
+    public void removeAccount(Account account) {
+        accounts.remove(account);
+    }
+
+    public void displayAccountBalances() {
+        for (Account account : accounts) {
+            System.out.println("Account ID: " + account.getId() + ", Balance: " + account.getBalance());
+        }
+    }
+
+    public void transfer(Account sourceAccount, Account destinationAccount, double amount) {
+        if (accounts.contains(sourceAccount) && accounts.contains(destinationAccount)) {
+            sourceAccount.withdraw(amount);
+            destinationAccount.deposit(amount);
+        }
+    }
+}
